d400066b64a591b4ba6c572ac8a7b0ae3278a105
刘邦 <bingfeng1225@163.com>
2018-05-15 10:55:52 +0800

    Update PWMeasureTextModule.java
    Modify one time to calculate multiple

--- android/src/main/java/com/qd/peiwen/measuretext/PWMeasureTextModule.java
+++ android/src/main/java/com/qd/peiwen/measuretext/PWMeasureTextModule.java
@@ -4,11 +4,13 @@ package com.qd.peiwen.measuretext;
 import android.graphics.Paint;
 import android.text.TextPaint;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.WritableArray;
 
 public class PWMeasureTextModule extends ReactContextBaseJavaModule {
 
@@ -25,15 +27,15 @@ public class PWMeasureTextModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void measure(ReadableMap options, Promise promise) {
-        TextPaint paint = new TextPaint();
-        paint.setAntiAlias(true);
-        if (!options.hasKey("fontSize")) {
-            promise.reject("ERR_INVALID_FONT", "missing required fontSize property");
-            return;
+    public void measure(ReadableArray array, Promise promise) {
+        WritableArray results = Arguments.createArray();
+        for (int i = 0;i< array.size();i++) {
+            TextPaint paint = new TextPaint();
+            paint.setAntiAlias(true);
+            paint.setTextSize((float) array.getDouble(i));
+            Paint.FontMetrics metrics = paint.getFontMetrics();
+            results.pushInt((int) Math.ceil(metrics.descent - metrics.top));
         }
-        paint.setTextSize((float) options.getDouble("fontSize"));
-        Paint.FontMetrics metrics = paint.getFontMetrics();
-        promise.resolve((int) Math.ceil(metrics.descent - metrics.top));
+        promise.resolve(results);
     }
 }
